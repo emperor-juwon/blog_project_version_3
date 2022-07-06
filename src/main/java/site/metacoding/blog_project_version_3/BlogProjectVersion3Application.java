3d27e973a14435f7c1a1f6e31171f17dd2d5ffc5
fwangjuwon <fwangjuwon@naver.com>
2022-07-06 13:58:04 +0900

    시큐리티 인증 필터 생성

---
+++ src/main/java/site/metacoding/blog_project_version_3/BlogProjectVersion3Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.blog_project_version_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectVersion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectVersion3Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/blog_project_version_3/BlogProjectVersion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.blog_project_version_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectVersion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
