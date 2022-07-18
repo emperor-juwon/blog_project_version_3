46811410afc665b6a3c40f300828e31cc9ddf2ad
emperor-juwon <fwangjuwon@gmail.com>
2022-07-18 22:32:57 +0900

    로그인 시 뷰 변경 및 로그인 세션 생성 완료

--- src/main/java/site/metacoding/blog_project_version_3/web/MainController.java
+++ src/main/java/site/metacoding/blog_project_version_3/web/MainController.java
@@ -1,17 +1,16 @@
 package site.metacoding.blog_project_version_3.web;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import site.metacoding.blog_project_version_3.config.auth.LoginUser;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
 public class MainController {
 
     @GetMapping({ "/" })
-    public String main(@AuthenticationPrincipal LoginUser loginUser) {
+    public String main() {
         return "main";
     }
 }
