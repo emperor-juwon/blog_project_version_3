42f989ec504461ecddc7708d9e0d724e743ba075
emperor-juwon <fwangjuwon@gmail.com>
2022-08-05 00:25:34 +0900

    로그 생성 완료

--- src/main/java/site/metacoding/blog_project_version_3/handler/GlobalExceptionHandler.java
+++ src/main/java/site/metacoding/blog_project_version_3/handler/GlobalExceptionHandler.java
@@ -5,21 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
 import site.metacoding.blog_project_version_3.handler.ex.CustomApiException;
 import site.metacoding.blog_project_version_3.handler.ex.CustomException;
 import site.metacoding.blog_project_version_3.util.Script;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomApiException.class)
     public ResponseEntity<?> apiException(Exception e) {
+        log.error("에러발생: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CustomException.class)
     public String htmlException(Exception e) {
-
+        log.error("에러발생: " + e.getMessage());
         return Script.back(e.getMessage());
     }
 
